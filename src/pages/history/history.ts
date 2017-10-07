2acdfe191a4c35447f1ee972ef590d83a7f28a1b
Giorgos Angelopoulos <agelo1995@gmail.com>
2017-10-07 14:11:52 +0300

    some colors added

--- src/pages/history/history.ts
+++ src/pages/history/history.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Http} from "@angular/http";
 
 /**
  * Generated class for the HistoryPage page.
@@ -13,13 +14,29 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   selector: 'page-history',
   templateUrl: 'history.html',
 })
-export class HistoryPage {
+export class HistoryPage implements OnInit{
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  tips:any;
+  sheetName: string = 'standard_history';
+  sheetID: string = '175XqnHhcOS8CVoBY08xMhaDO39VmauBEFKU9qeaHS3U';
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,private http:Http) {
+
   }
 
-  ionViewDidLoad() {
+
+  ionViewWillEnter() {
     console.log('ionViewDidLoad HistoryPage');
+
   }
 
+  ngOnInit(){
+    console.log('https://script.google.com/macros/s/AKfycbygukdW3tt8sCPcFDlkMnMuNu9bH5fpt7bKV50p2bM/exec?id='+this.sheetID+'&sheet='+this.sheetName);
+    this.http.get('https://script.google.com/macros/s/AKfycbygukdW3tt8sCPcFDlkMnMuNu9bH5fpt7bKV50p2bM/exec?id='+this.sheetID+'&sheet='+this.sheetName)
+      .map(res => res.json()).subscribe(data => {
+      this.tips = data.standard_history;
+      console.log(this.tips);
+    })
+  }
 }
